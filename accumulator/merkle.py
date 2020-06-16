97c1051611a032cf22524933836f670b0fc0be45
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2020-06-16 22:40:26 +0200

    Added Merkle proof/verify

--- accumulator/merkle.py
+++ accumulator/merkle.py
@@ -1,17 +1,18 @@
 from .common import NIL, H
+from typing import List
 
 # A dynamic Merkle tree. Can add leafs or change existing leafs, but cannot remove leafs.
 # Leafs are already assumed to be in the domain of the hash function. Each internal node is the hash of the concatenation of the two leaves.
 
 # for a tree with n leafs, with adding a leaf is amortized log(n), editing is worst case log(n)
 
-def parent(n: int):
+def parent(n: int) -> int:
     return (n - 1) // 2
 
-def left_child(n: int):
+def left_child(n: int) -> int:
     return 2 * n + 1
 
-def right_child(n: int):
+def right_child(n: int) -> int:
     return 2 * n + 2
 
 class MerkleTree:
@@ -20,27 +21,27 @@ class MerkleTree:
         self.capacity = 1 # current capacity; always a power of two
         self.nodes = [NIL]
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Returns the total number of leafs in the tree."""
         return self.k
 
     @property
-    def first_leaf(self):
+    def first_leaf(self) -> int:
         return self.capacity - 1
 
-    def fix_node(self, i: int):
+    def fix_node(self, i: int) -> None:
         self.nodes[i] = H(self.nodes[left_child(i)] + self.nodes[right_child(i)])
 
-    def fix_up(self, i: int):
+    def fix_up(self, i: int) -> None:
         while i > 0:
             i = parent(i)
             self.fix_node(i)
 
-    def recompute_internal_nodes(self):
+    def recompute_internal_nodes(self) -> None:
         for i in range(self.first_leaf - 1, -1, -1):
             self.fix_node(i)
 
-    def double_capacity(self):
+    def double_capacity(self) -> None:
         initial_capacity = self.capacity
         initial_first_leaf = self.first_leaf
         self.capacity *= 2
@@ -50,17 +51,43 @@ class MerkleTree:
 
         self.recompute_internal_nodes()
 
-    def add(self, x: bytes):
+    def add(self, x: bytes) -> None:
         if self.k == self.capacity:
             self.double_capacity()
 
         self.k += 1
         self.set(self.k - 1, x)
 
-    def set(self, i: int, x: bytes):
-        leaf = self.first_leaf + i
+    def set(self, index: int, x: bytes) -> None:
+        leaf = self.first_leaf + index
         self.nodes[leaf] = x
         self.fix_up(leaf)
 
-    def get(self, i: int):
+    def get(self, i: int) -> bytes:
         return self.nodes[self.first_leaf + i]
+
+    def prove_leaf(self, index: int) -> List[bytes]:
+        i = self.first_leaf + index
+        proof = [self.nodes[i]]
+        while i > 0:
+            if index % 2 == 0:
+                sibling = i + 1
+            else:
+                sibling = i - 1
+            proof.append(self.nodes[sibling])
+
+            i = parent(i)
+            index = index // 2
+
+        return proof
+
+
+def merkle_proof_verify(root: bytes, proof: List[bytes]) -> bool:
+    if len(proof) < 1:
+        return False
+
+    h = proof[0]
+    for l in proof[1:]:
+        h = H(h + l)
+
+    return h == root
