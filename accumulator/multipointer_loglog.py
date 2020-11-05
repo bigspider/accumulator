fe5d145a1dde8014af38a30ca5ce76aec97ec292
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2020-11-05 15:06:41 +0700

    Added special case loglogn of multipointer accumulator

---
+++ accumulator/multipointer_loglog.py
@@ -0,0 +1,31 @@
+from .common import is_power_of_2, zeros, pred
+from .generalized_accumulator import GeneralizedAccumulatorFactory, GeneralizedAccumulator, GeneralizedProver, GeneralizedVerifier
+
+# Implementation of the generalized accumulator with about log log n "evenly spaced" back-pointers.
+
+# Cost of update: O(log log n)
+# Proof size: O(log n (log log n)^2)
+
+def get_representatives(k: int):
+    if k == 1:
+        return []
+
+    # if k is even, we also add k - 1
+    result = [k - 1] if k % 2 == 0 else []
+
+    t = k
+    c = 1
+    while t > 0:
+        t = pred(t)
+        if is_power_of_2(c):
+            result.append(t)
+        c += 1
+    return result
+
+
+class MultipointerLogLogFactory(GeneralizedAccumulatorFactory):
+    def create_accumulator(self):
+        accumulator_manager = GeneralizedAccumulator(get_representatives)
+        prover = GeneralizedProver(get_representatives, accumulator_manager)
+        verifier = GeneralizedVerifier(get_representatives)
+        return accumulator_manager, prover, verifier
---
+++ tests/test_multipointer_loglog.py
@@ -0,0 +1,45 @@
+import unittest
+from accumulator.multipointer_loglog import get_representatives, MultipointerLogLogFactory
+from accumulator.common import pred
+
+from .base import BaseAccumulatorTestSuite
+
+class GeneralizedAccumulatorTestSuite(BaseAccumulatorTestSuite, unittest.TestCase):
+    """Generalized accumulator test cases."""
+
+    def test_get_representatives(self):
+        self.assertEqual(get_representatives(1), [])
+
+        # self.assertEqual(get_representatives(2), [1])
+        # self.assertEqual(get_representatives(3), [2])
+        # self.assertEqual(get_representatives(4), [3])
+        # self.assertEqual(get_representatives(5), [4])
+        # self.assertEqual(get_representatives(6), [5, 4])
+        # self.assertEqual(get_representatives(7), [6])
+        # self.assertEqual(get_representatives(8), [7])
+        # self.assertEqual(get_representatives(9), [8])
+        # self.assertEqual(get_representatives(10), [9, 8])
+        # self.assertEqual(get_representatives(11), [10])
+        # self.assertEqual(get_representatives(12), [11, 8])
+        # self.assertEqual(get_representatives(13), [12])
+        # self.assertEqual(get_representatives(14), [13, 12])
+        # self.assertEqual(get_representatives(15), [14])
+        # self.assertEqual(get_representatives(16), [15])
+        # self.assertEqual(get_representatives(17), [16])
+        # self.assertEqual(get_representatives(18), [17, 16])
+
+        # self.assertEqual(get_representatives(1), [])
+        # self.assertEqual(get_representatives(32), [31])
+        # self.assertEqual(get_representatives(33), [32])
+        # self.assertEqual(get_representatives(39), [38, 36])
+        # self.assertEqual(get_representatives(40), [39, 32])
+        # self.assertEqual(get_representatives(42), [41, 40, 32])
+
+
+    def get_instances(self):
+        factory = MultipointerLogLogFactory()
+        return factory.create_accumulator()
+
+
+if __name__ == '__main__':
+    unittest.main()
